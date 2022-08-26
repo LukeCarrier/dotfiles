5608af77e1828e19c5d2923128010696b4eefed3
Luke Carrier <luke@carrier.im>
2022-08-26 16:31:47 +0100

    Boilerplate

---
+++ README.md
@@ -0,0 +1,27 @@
+# My dotfiles
+
+Managed with GNU Stow this time around.
+
+---
+
+## Setup
+
+macOS:
+
+```console
+brew install stow
+```
+
+## Usage
+
+Install the default set of packages:
+
+```console
+./install.sh
+```
+
+Install specific packages:
+
+```console
+./install.sh -p package1 -p package2
+```
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+default_packages=(
+)
+
+packages=()
+while true; do
+  case "$1" in
+    -p|--package) packages+="$2" ; shift 2 ;;
+    *           ) break                    ;;
+  esac
+done
+
+set -eu -o pipefail
+
+if (( ${#packages[@]} == 0 )); then
+  packages=( ${default_packages[@]} )
+fi
+
+for package in "${packages[@]}"; do
+  echo "Stowing $package to $HOME"
+  stow --restow --target "$HOME" "$package"
+done
