4654de3ce139689a33e0955d940e8c14920322cf
Luke Carrier <luke.carrier@babylonhealth.com>
2024-08-02 08:59:11 +0100

    Trialing WezTerm
    It turns out that the combination of a terminal emulator, terminal
    multiplexer, and command palette is pretty awesome.

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.window_frame = {
+  font = wezterm.font { family = 'SF Pro Display' },
+}
+config.window_background_opacity = 0.85
+
+config.command_palette_font_size = 18.0
+
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+
+config.font = wezterm.font('MonaSpiceKr NF')
+config.font_size = 14.0
+
+config.color_scheme = 'TokyoNightStorm'
+config.colors = { background = '#1a1b26' }
+
+return config
