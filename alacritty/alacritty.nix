9300cfc0066e9a57206eecd721d17d3194f57467
Luke Carrier <luke@carrier.family>
2024-09-25 00:15:20 +0100

    alacritty: properly theme it

--- alacritty/alacritty.nix
+++ alacritty/alacritty.nix
@@ -1,4 +1,33 @@
 { config, pkgs, ... }:
 {
-  home.packages = with pkgs; [ alacritty ];
+  home.file = {
+    "~/.config/alacritty/themes/tokyo-night-storm.toml".source
+      = ./.config/alacritty/themes/tokyo-night-storm.toml;
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      import = [
+        "~/.config/alacritty/themes/tokyo-night-storm.toml"
+      ];
+
+      colors.draw_bold_text_with_bright_colors = true;
+
+      font = {
+        size = 12;
+        normal.family = "JetBrainsMono Nerd Font";
+      };
+
+      mouse.hide_when_typing = true;
+
+      scrolling.history = 0;
+
+      window = {
+        dynamic_padding = true;
+        dynamic_title = true;
+        option_as_alt = "both";
+      };
+    };
+  };
 }
