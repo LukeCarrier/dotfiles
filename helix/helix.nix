b6b5ce86849c97b302a77766a5eeca86c6accf3d
Luke Carrier <luke@carrier.family>
2024-10-14 22:26:29 +0100

    helix: port

---
+++ helix/helix.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+{
+  home.file."${config.xdg.configHome}/helix/themes/local_tokyonight_storm.toml".source =
+      ./.config/helix/themes/local_tokyonight_storm.toml;
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "local_tokyonight_storm";
+      editor = {
+        color-modes = true;
+        cursorline = true;
+        line-number = "relative";
+
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+
+        file-picker.hidden = false;
+        indent-guides.render = true;
+        soft-wrap.enable = true;
+      };
+    };
+  };
+}
