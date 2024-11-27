7b105073edfec1a8ae9c6f8a49d48b3d906571f1
Luke Carrier <luke@carrier.family>
2024-11-27 10:19:24 +0000

    B-4653: trust some cachix caches

--- host/b-4653/b-4653.nix
+++ host/b-4653/b-4653.nix
@@ -2,6 +2,17 @@
 {
   system.stateVersion = 5;
 
+  nix.settings = {
+    trusted-substituters = [
+      "https://nixpkgs-python.cachix.org"
+      "https://nixpkgs-terraform.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+      "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw="
+    ];
+  };
+
   networking.computerName = "B-4653";
   networking.hostName = "B-4653.hq.babylonhealth.com";
   networking.localHostName = "B-4653";
