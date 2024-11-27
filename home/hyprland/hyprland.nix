8cd052d5a58ed969d6e3657ade64d21b700e2053
Luke Carrier <luke@carrier.family>
2024-11-27 10:19:23 +0000

    hyprland: Michael's dock setup

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -71,6 +71,7 @@
     package = pkgs-unstable.kanshi;
     systemdTarget = "hyprland-session.target";
     settings = [
+      # Laptop's own display
       {
         output.criteria = "eDP-1";
         output.mode = "2880x1920@120Hz";
@@ -78,6 +79,8 @@
         output.scale = 1.25;
         output.transform = null;
       }
+
+      # My office
       {
         output.criteria = "Samsung Electric Company U32J59x HTPK702789";
         output.mode = "3840x2160@60Hz";
@@ -92,6 +95,16 @@
         output.scale = 1.25;
         output.transform = null;
       }
+
+      # Michael's office
+      {
+        output.criteria = "Ancor Communications Inc ASUS VS247 C8LMTF177755";
+        output.mode = "1920x1080@60Hz";
+        output.adaptiveSync = false;
+        output.scale = 1.0;
+        output.transform = null;
+      }
+
       {
         profile.name = "mobile";
         profile.outputs = [
@@ -138,6 +151,21 @@
             criteria = "eDP-1";
             status = "enable";
             position = "4224,1728";
+          }
+        ];
+      }
+      {
+        profile.name = "peacehavenMichaelDocked";
+        profile.outputs = [
+          {
+            criteria = "Ancor Communications Inc ASUS VS247 C8LMTF177755";
+            status = "enable";
+            position = "0,0";
+          }
+          {
+            criteria = "eDP-1";
+            status = "enable";
+            position = "1920,540";
           }
         ];
       }
