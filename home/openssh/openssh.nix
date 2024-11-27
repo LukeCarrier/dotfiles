d190b3c5f7916402c381473c3f56e51d7995903a
Luke Carrier <luke@carrier.family>
2024-11-27 10:19:24 +0000

    openssh: cleanup

--- home/openssh/openssh.nix
+++ home/openssh/openssh.nix
@@ -3,6 +3,6 @@
   home.packages = with pkgs; [ openssh ];
 
   programs.ssh.enable = true;
-  # Use Gnome Keyring instead
+  # Use Gnome Keyring (Linux) or a password manager (macOS) instead
   services.ssh-agent.enable = false;
 }
