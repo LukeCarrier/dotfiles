382377a5e241512a948122327602e986611feacb
Luke Carrier <luke@carrier.family>
2024-11-27 10:19:23 +0000

    .: start porting to home-manager

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Home Manager configuration of lukecarrier";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{
+    home-manager,
+    nixpkgs,
+    nixpkgs-unstable,
+    ...
+  }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."lukecarrier" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        extraSpecialArgs = {
+          pkgs-unstable = import nixpkgs-unstable {
+            inherit system;
+          };
+          gitConfig.user.signingKey = "1CBBEBFE0CDC1C06DB324A7CCE439AFEC33D9E7F";
+        };
+
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
+}
