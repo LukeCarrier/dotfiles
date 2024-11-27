10930e49cfde62dc3c4b8acf06c75c648bc5a3a8
Luke Carrier <luke@carrier.family>
2024-11-27 10:19:23 +0000

    hyprland: configure Kanshi for monitor arrangement

--- hyprland/hyprland.nix
+++ hyprland/hyprland.nix
@@ -1,9 +1,9 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
 {
   home.packages = with pkgs; [
     wl-clipboard
     wf-recorder
-    kanshi
+    pkgs-unstable.kanshi
     mako
     grim
     slurp
@@ -36,15 +36,90 @@
     name = "Adwaita";
   };
 
+  services.kanshi = {
+    enable = true;
+    package = pkgs-unstable.kanshi;
+    systemdTarget = "hyprland-session.target";
+    settings = [
+      {
+        output.criteria = "eDP-1";
+        output.mode = "2880x1920@120Hz";
+        output.adaptiveSync = true;
+        output.scale = 1.25;
+        output.transform = null;
+      }
+      {
+        output.criteria = "Samsung Electric Company U32J59x HTPK702789";
+        output.mode = "3840x2160@60Hz";
+        output.adaptiveSync = false;
+        output.scale = 1.25;
+        output.transform = null;
+      }
+      {
+        output.criteria = "Samsung Electric Company U32J59x HTPK602008";
+        output.mode = "3840x2160@30Hz";
+        output.adaptiveSync = false;
+        output.scale = 1.25;
+        output.transform = null;
+      }
+      {
+        profile.name = "mobile";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            status = "enable";
+            position = "0,0";
+          }
+        ];
+      }
+      {
+        profile.name = "peacehavenDockedClosed";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+          {
+            criteria = "Samsung Electric Company U32J59x HTPK702789";
+            status = "enable";
+            position = "0,0";
+          }
+          {
+            criteria = "Samsung Electric Company U32J59x HTPK602008";
+            status = "enable";
+            position = "3072,0";
+          }
+        ];
+      }
+      {
+        profile.name = "peacehavenDockedOpen";
+        profile.outputs = [
+          {
+            criteria = "Samsung Electric Company U32J59x HTPK702789";
+            status = "enable";
+            position = "0,0";
+          }
+          {
+            criteria = "Samsung Electric Company U32J59x HTPK602008";
+            status = "enable";
+            position = "3072,0";
+          }
+          {
+            criteria = "eDP-1";
+            status = "enable";
+            position = "4224,1728";
+          }
+        ];
+      }
+    ];
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
       "$menu" = "wofi --allow-images --show drun";
       "$fileManager" = "nautilus";
       "$terminal" = "xterm";
-      monitor = [
-        "eDP-1, 2880x1920@120, 0x0, 1.5"
-      ];
       env = [
         "XDG_CURRENT_DESKTOP,Hyprland"
         "XDG_SESSION_TYPE,wayland"
