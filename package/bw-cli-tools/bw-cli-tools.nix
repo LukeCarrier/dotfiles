1cb656a1e436b55fe8ffa4c1a45d26c744eb5a8e
Luke Carrier <luke@carrier.family>
2024-11-29 14:46:14 +0000

    bw-cli-tools: bw-ssh{ => -add }*

--- package/bw-cli-tools/bw-cli-tools.nix
+++ package/bw-cli-tools/bw-cli-tools.nix
@@ -1,17 +1,17 @@
 { pkgs }:
 let
   bwSession = pkgs.writeShellScriptBin "bw-session" (builtins.readFile ./bw-session);
-  bwSsh = pkgs.writeShellScriptBin "bw-ssh" ''
+  bwSshAdd = pkgs.writeShellScriptBin "bw-ssh-add" ''
     entry="$1"
     file="$2"
     bw get attachment "$file" --itemid "$entry" --raw \
-      | BW_PASSWORD="$entry" SSH_ASKPASS="${bwSshAskpass}/bin/bw-ssh-askpass" ssh-add -
+      | BW_PASSWORD="$entry" SSH_ASKPASS="${bwSshAddAskpass}/bin/bw-ssh-add-askpass" ssh-add -
   '';
-  bwSshAskpass = pkgs.writeShellScriptBin "bw-ssh-askpass" ''
+  bwSshAddAskpass = pkgs.writeShellScriptBin "bw-ssh-add-askpass" ''
     bw get password "$BW_PASSWORD"
   '';
 in pkgs.symlinkJoin {
   pname = "bw-cli-tools";
   version = "0.1.0";
-  paths = [ bwSession bwSsh bwSshAskpass ];
+  paths = [ bwSession bwSshAdd bwSshAddAskpass ];
 }
