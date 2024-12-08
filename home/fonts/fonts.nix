3dfc4491204659272ab0f4474c7299b07d724bc2
Luke Carrier <luke@carrier.family>
2024-12-08 17:42:28 +0000

    nix flake update

--- home/fonts/fonts.nix
+++ home/fonts/fonts.nix
@@ -3,14 +3,13 @@
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
-    (nerdfonts.override { fonts = ["Monaspace"]; })
+    nerd-fonts.monaspace
 
     fira-code
     fira-code-symbols
     font-awesome
     liberation_ttf
     mplus-outline-fonts.githubRelease
-    nerdfonts
     noto-fonts
     noto-fonts-emoji
     proggyfonts
