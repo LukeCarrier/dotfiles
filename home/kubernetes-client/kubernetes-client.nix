4dcbc39ff58083beaa9f3fd013c7924704358918
Luke Carrier <luke@carrier.family>
2024-12-13 21:52:59 +0000

    kubernetes-client: replace kubectx with an alias

--- home/kubernetes-client/kubernetes-client.nix
+++ home/kubernetes-client/kubernetes-client.nix
@@ -1,8 +1,12 @@
 { config, pkgs, ... }:
+let
+  selectConfigCmd = ''
+    fzf --walker file --walker-root "$HOME/.kube/configs" --preview 'cat {}' --prompt "Kubernetes configuration (currently $KUBECONFIG)"
+  '';
+in
 {
   home.packages = with pkgs; [
     kubectl
-    kubectx
     krew
     kyverno
   ];
@@ -12,6 +16,17 @@
   ];
 
   home.file = {
+    ".kube/config".text = "";
     ".kube/go-template/resources.gotmpl".source = ./go-template/resources.gotmpl;
   };
+
+  programs.bash.shellAliases.kube-config = ''
+    export KUBECONFIG="$(${selectConfigCmd})"
+    echo "Selected $KUBECONFIG"
+  '';
+  programs.fish.functions.kube-config.body = ''
+    export KUBECONFIG=(${selectConfigCmd})
+    echo "Selected $KUBECONFIG"
+  '';
+  programs.zsh.shellAliases.kube-config = config.programs.bash.shellAliases.kube-config;
 }
