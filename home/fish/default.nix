88a3d8c02fdc4e46af31a1c0422c7c50976cc9c5
Luke Carrier <luke@carrier.family>
2024-12-23 14:49:36 +0000

    fish: fix default shell on Zsh

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -5,7 +5,7 @@
 
   programs.bash.initExtra = ''
     if [[ $- == *i* ]] && [[ $(ps -p $PPID -o comm=) != "fish" ]] && [[ -z ''${BASH_EXECUTION_STRING} ]]; then
-      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=
+      [[ $- == *l* ]] && LOGIN_OPTION='--login' || LOGIN_OPTION=
       exec fish $LOGIN_OPTION
     fi
   '';
