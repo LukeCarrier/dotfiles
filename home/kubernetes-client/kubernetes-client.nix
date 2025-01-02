a52ab0387738ba5c9655e034f6b58d2ca874c51b
Luke Carrier <luke@carrier.family>
2025-01-02 13:18:28 +0000

    kubernetes-client: kube-context

--- home/kubernetes-client/kubernetes-client.nix
+++ home/kubernetes-client/kubernetes-client.nix
@@ -1,8 +1,31 @@
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
-  selectConfigCmd = ''
-    fzf --walker file --walker-root "$HOME/.kube/configs" --delimiter / --with-nth -1 --preview 'cat {}' --prompt "Kubernetes configuration (currently $KUBECONFIG)"
-  '';
+  selectCmds = {
+    config = rec {
+      env = "KUBECONFIG";
+      optionCmd =
+        ''fzf --walker file --walker-root "$HOME/.kube/configs" --delimiter / --with-nth -1 --preview 'cat {}' --prompt "Kubernetes configuration (currently $''
+        + env
+        + '')"'';
+    };
+    context = rec {
+      env = "KUBECTX";
+      optionCmdFish =
+        ''kubectl config get-contexts --output name | fzf --preview-window wrap --preview 'kubectl config view --output jsonpath=\'{.contexts[?(@.name == "{}")]}\''' --prompt "Kubernetes context (currently $''
+        + env
+        + '')"'';
+      optionCmd =
+        ''kubectl config get-contexts --output name | fzf --preview-window wrap --preview 'kubectl config view --output jsonpath='{.contexts[?(@.name == "{}")]}''' --prompt "Kubernetes context (currently $''
+        + env
+        + '')"'';
+      setCmd = ''kubectl config use-context "$'' + env + ''"'';
+    };
+  };
 in
 {
   home.packages = with pkgs; [
@@ -19,23 +42,40 @@ in
     ".kube/go-template/resources.gotmpl".source = ./go-template/resources.gotmpl;
   };
 
-  programs.bash.initExtra = ''
-    kube-config() {
-      if [[ -n "$1" ]]; then
-        export KUBECONFIG="$1"
-      else
-        export KUBECONFIG="$(${selectConfigCmd})"
-      fi
-      echo "Selected $KUBECONFIG"
+  programs.bash.initExtra = lib.strings.concatStringsSep "\n" (
+    lib.attrsets.attrValues (
+      lib.attrsets.mapAttrs (
+        name: options:
+        ''
+          kube-${name}() {
+            if [[ -n "$1" ]]; then
+              export ${options.env}="$1"
+            else
+              export ${options.env}="$(${options.optionCmd})"
+            fi
+            echo "Selected $''
+        + options.env
+        + ''"
+          }
+        ''
+      ) selectCmds
+    )
+  );
+  programs.fish.functions = lib.attrsets.mapAttrs' (
+    name: options:
+    lib.attrsets.nameValuePair "kube-${name}" {
+      body =
+        ''
+          if test -n "$argv[1]"
+            export ${options.env}=$argv[1]
+          else
+            export ${options.env}=(${options.optionCmdFish or options.optionCmd})
+          end
+          ${options.setCmd or ""}
+          echo "Selected $''
+        + options.env
+        + ''"'';
     }
-  '';
-  programs.fish.functions.kube-config.body = ''
-    if test -n "$argv[1]"
-      export KUBECONFIG=$argv[1]
-    else
-      export KUBECONFIG=(${selectConfigCmd})
-    end
-    echo "Selected $KUBECONFIG"
-  '';
+  ) selectCmds;
   programs.zsh.initExtra = config.programs.bash.initExtra;
 }
