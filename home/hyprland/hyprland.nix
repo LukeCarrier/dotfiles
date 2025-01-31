abe3d188a7d9d7d533e2229ab778b8cee8e9021b
Luke Carrier <luke@carrier.family>
2025-01-31 16:51:20 +0000

    hyprland: properly wire up env

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -237,7 +237,6 @@
       };
       env = [
         "TERM,wezterm"
-        "XDG_CURRENT_DESKTOP,Hyprland"
         "XDG_SESSION_TYPE,wayland"
         "XDG_SESSION_DESKTOP,Hyprland"
         "CLUTTER_BACKEND,wayland"
