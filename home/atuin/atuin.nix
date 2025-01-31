ac999dffe40f34ec37f8ef05030b08f8ca8af1fb
Luke Carrier <luke@carrier.family>
2025-01-31 15:32:55 +0000

    atuin: make it suck less

--- home/atuin/atuin.nix
+++ home/atuin/atuin.nix
@@ -1,4 +1,12 @@
 { ... }:
 {
-  programs.atuin.enable = true;
+  programs.atuin = {
+    enable = true;
+    settings = {
+      enter_accept = false;
+      filter_mode = "host";
+      filter_mode_shell_up_key_binding = "session";
+      keymap_mode = "vim-normal";
+    };
+  };
 }
