63417e11899711d924508efc9b8a3568427256c1
Luke Carrier <luke@carrier.family>
2025-03-12 12:15:57 +0000

    bitwarden: set SSH agent socket path

--- home/bitwarden/bitwarden.nix
+++ home/bitwarden/bitwarden.nix
@@ -2,6 +2,8 @@
 {
   home.packages = [ pkgs.bitwarden-desktop ];
 
+  home.sessionVariables.BITWARDEN_SSH_AUTH_SOCK = "$HOME/.bitwarden/ssh-agent.sock";
+
   programs.ssh.matchBlocks.all = {
     match = "all";
     extraOptions = {
