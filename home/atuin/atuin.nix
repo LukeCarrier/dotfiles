bbec8d346c8c5df1b54352d8171f9c9a20f65bf4
Luke Carrier <luke@carrier.family>
2025-03-24 09:42:27 +0000

    atuin: insert mode by default

--- home/atuin/atuin.nix
+++ home/atuin/atuin.nix
@@ -6,7 +6,7 @@
       enter_accept = false;
       filter_mode = "host";
       filter_mode_shell_up_key_binding = "session";
-      keymap_mode = "vim-normal";
+      keymap_mode = "vim-insert";
     };
   };
 }
