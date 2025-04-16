3a106499be411bfc6e000049d378794382f45e4a
Luke Carrier <luke@carrier.family>
2025-04-16 09:50:55 +0100

    wezterm: on Darwin, ship link to app bundle

--- home/wezterm/wezterm.nix
+++ home/wezterm/wezterm.nix
@@ -1,7 +1,21 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.wezterm = {
     enable = true;
+    # On macOS we expect the app bundle in /Applications, installed via
+    # Homebrew cask or manually. Lots of things seem broken using the nixpkgs
+    # executable, and it's harder to launch.
+    package = (
+      if pkgs.stdenv.isDarwin
+      then (
+        pkgs.runCommand "wezterm-darwin" {
+        } ''
+          mkdir -p "$out/bin"
+          ln -sf "/Applications/WezTerm.app/Contents/MacOS/wezterm" "$out/bin/wezterm"
+        ''
+      )
+      else pkgs.wezterm
+    );
     enableBashIntegration = true;
     enableZshIntegration = true;
     extraConfig = builtins.readFile ./.wezterm.lua;
