ff1a824274caffd97f5701a8e24a48fef6660511
Luke Carrier <luke@carrier.family>
2025-04-27 12:42:02 +0100

    swayidle: don't rely on tools being in home.packages
    The before-sleep command's failure will actually inhibit sleep, making
    for a rather toasty pile of electronics.

--- home/swayidle/swayidle.nix
+++ home/swayidle/swayidle.nix
@@ -1,28 +1,32 @@
-{ ... }:
-{
+{ pkgs, ... }:
+let
+  brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+  loginctl = "${pkgs.systemd}/bin/loginctl";
+  systemctl = "${pkgs.systemd}/bin/systemctl";
+in {
   services.swayidle = {
     enable = true;
 
     events = [
       {
         event = "before-sleep";
-        command = "loginctl lock-session";
+        command = "${loginctl} lock-session";
       }
     ];
 
     timeouts = [
       {
         timeout = 60;
-        command = "brightnessctl -sd rgb:kbd_backlight set 0";
-        resumeCommand = "brightnessctl -rd rgb:kbd_backlight";
+        command = "${brightnessctl} -sd rgb:kbd_backlight set 0";
+        resumeCommand = "${brightnessctl} -rd rgb:kbd_backlight";
       }
       {
         timeout = 120;
-        command = "loginctl lock-session";
+        command = "${loginctl} lock-session";
       }
       {
         timeout = 300;
-        command = "systemctl suspend-then-hibernate";
+        command = "${systemctl} suspend-then-hibernate";
       }
     ];
   };
