0f79e3409adc25ef319ed5db88b2caa9dec0a172
Luke Carrier <luke@carrier.family>
2025-04-29 09:57:13 +0100

    component/fish: ugly hack for OpenCode.app

--- component/fish/default.nix
+++ component/fish/default.nix
@@ -3,15 +3,17 @@
   # Fish isn't POSIX compliant, so making it the user shell is ill advised.
   # Has to tolerate stupid macOS ps.
 
-  programs.bash.initExtra = ''
-    if [[ $- == *i* ]] && [[ $(ps -p $PPID -o comm=) != "fish" ]] && [[ -z ''${BASH_EXECUTION_STRING} ]]; then
+  programs.zsh.initContent = ''
+    parent_comm=$(ps -p $PPID -o comm=)
+    if [[ $- == *i* ]] && [[ "$parent_comm" != "fish" ]] && [[ "$parent_comm" != *"OpenCode"* ]] && [[ -z ''${ZSH_EXECUTION_STRING} ]]; then
       [[ $- == *l* ]] && LOGIN_OPTION='--login' || LOGIN_OPTION=
       exec fish $LOGIN_OPTION
     fi
   '';
 
-  programs.zsh.initContent = ''
-    if [[ $- == *i* ]] && [[ $(ps -p $PPID -o comm=) != "fish" ]] && [[ -z ''${ZSH_EXECUTION_STRING} ]]; then
+  programs.bash.initExtra = ''
+    parent_comm=$(ps -p $PPID -o comm=)
+    if [[ $- == *i* ]] && [[ "$parent_comm" != "fish" ]] && [[ "$parent_comm" != *"OpenCode"* ]] && [[ -z ''${BASH_EXECUTION_STRING} ]]; then
       [[ $- == *l* ]] && LOGIN_OPTION='--login' || LOGIN_OPTION=
       exec fish $LOGIN_OPTION
     fi
