50856fd8bcecce48504235190e94bf20ef29ad44
Luke Carrier <luke@carrier.family>
2025-05-01 10:50:24 +0100

    emed: monoweb hosts

--- employer/emed/darwin.nix
+++ employer/emed/darwin.nix
@@ -1,5 +1,16 @@
 { ... }:
 {
+  # FIXME: implement nix-darwin/nix-darwin#1035
+  # networking.extraHosts = ''
+  #   # babylonhealth/monoweb
+  #   127.0.0.1 localhost.docker.babylontech.co.uk
+  #   127.0.0.1 localhost.dev.babylontech.co.uk
+  #   127.0.0.1 localhost.staging.babylontech.co.uk
+  #   127.0.0.1 localhost.preprod.babylontech.co.uk
+  #   127.0.0.1 localhost.ca.preprod.babylontech.co.uk
+  #   127.0.0.1 localhost.sg.preprod.babylontech.co.uk
+  # '';
+
   homebrew = {
     brews = [
       "gh"
