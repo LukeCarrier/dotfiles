a8b8894ad43cb88c6f01708c54f148ab23be5bed
Luke Carrier <luke@carrier.family>
2025-09-01 22:13:11 +0100

    nix flake update

--- component/openssh/openssh.nix
+++ component/openssh/openssh.nix
@@ -2,7 +2,23 @@
 {
   home.packages = [ pkgs.openssh ];
 
-  programs.ssh.enable = true;
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+
+    matchBlocks."*" = {
+      forwardAgent = "no";
+      addKeysToAgent = "no";
+      compression = "no";
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = "no";
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
+  };
 
   # Use Gnome Keyring (Linux) or a password manager (macOS) instead
   services.ssh-agent.enable = false;
