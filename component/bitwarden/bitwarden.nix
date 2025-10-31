70ba4f66fc2fc669db4d3a1302073ac11a3e33b4
Luke Carrier <luke@carrier.family>
2025-10-31 14:43:34 +0000

    bitwarden: CLI-only on Darwin

--- component/bitwarden/bitwarden.nix
+++ component/bitwarden/bitwarden.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    bitwarden-desktop
-    bitwarden-cli
-    bw-cli-tools
-  ];
+  home.packages =
+    with pkgs;
+    [
+      bitwarden-cli
+      bw-cli-tools
+    ]
+    ++ (if pkgs.stdenv.isLinux then [ pkgs.bitwarden-desktop ] else [ ]);
 
   home.sessionVariables.BITWARDEN_SSH_AUTH_SOCK = "$HOME/.bitwarden/ssh-agent.sock";
 
