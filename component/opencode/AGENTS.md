30e8c40da2791171fe42986a02adc17816b10015
Luke Carrier <luke@carrier.family>
2025-11-07 15:41:56 +0000

    component/opencode, secrets: start configuring it a little

---
+++ component/opencode/AGENTS.md
@@ -0,0 +1,10 @@
+# Ground rules
+
+- No need for a preppy, sickly tone.
+  - No psychophantic "you're absolutely right".
+- Do not query MCPs unless granted express permission. You may request it.
+- Keep answers focused and on task.
+- If you are not confident you understand the question, seek clarification early.
+- If you are not confident you know something, research early.
+- Be consise in responses. I will ask for clarification when necessary.
+- Refrain from "add 1 to i" style comments.
--- component/opencode/opencode.nix
+++ component/opencode/opencode.nix
@@ -1,6 +1,46 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+let
+  userFacingPkgs = with pkgs; [
+    container-use
+    mcp-remote
+  ];
+  wrapperPkgs = with pkgs; [
+    emcee
+    github-mcp-server
+    terraform-mcp-server
+  ];
+  opencode = pkgs.symlinkJoin {
+    name = "opencode-wrapped";
+    paths = [ pkgs.opencode ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/opencode \
+        --prefix PATH : ${pkgs.lib.makeBinPath wrapperPkgs}
+    '';
+  };
+in
 {
-  home.packages = [ pkgs.opencode ];
+  home.packages = [ opencode ] ++ userFacingPkgs;
 
-  home.file.".config/opencode/opencode.json".source = ./opencode.json;
+  sops = {
+    secrets.opencode-github-token = {
+      sopsFile = ../../secrets/personal.yaml;
+      format = "yaml";
+      key = "opencode/github";
+    };
+
+    templates."opencode.jsonc" = {
+      content =
+        builtins.replaceStrings
+          [ "@GITHUB_PERSONAL_ACCESS_TOKEN@" ]
+          [ config.sops.placeholder.opencode-github-token ]
+          (builtins.readFile ./opencode.jsonc.template);
+      path = "${config.home.homeDirectory}/.config/opencode/opencode.jsonc";
+    };
+  };
+
+  home.file = {
+    ".config/opencode/AGENTS.md".source = ./AGENTS.md;
+    ".config/opencode/agent/container-use.md".source = ./agent/container-use.md;
+  };
 }
