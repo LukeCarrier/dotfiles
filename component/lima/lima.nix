56fa945be3ae73d3c9537218b7f631b408b58e1a
Luke Carrier <luke@carrier.family>
2026-01-01 20:58:44 +0000

    component/lima: include VM SSH configs
    This allows easy `ssh lima-$vm`.

--- component/lima/lima.nix
+++ component/lima/lima.nix
@@ -9,4 +9,6 @@
   home.file.".lima/docker-amd64/lima.yaml".source = ./.lima/docker-amd64/lima.yaml;
   home.file.".lima/docker-amd64-rosetta/lima.yaml".source = ./.lima/docker-amd64-rosetta/lima.yaml;
   home.file.".lima/docker-arm64/lima.yaml".source = ./.lima/docker-arm64/lima.yaml;
+
+  programs.ssh.includes = [ "~/.lima/*/ssh.config" ];
 }
