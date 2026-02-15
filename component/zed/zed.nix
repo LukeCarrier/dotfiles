4fed86b8b7032e952855b6ba34888d1d68e0e24b
Luke Carrier <luke@carrier.family>
2026-02-15 15:39:25 +0000

    component/zed: options clean up
    Because of how existing options are merged into the new configuration
    I'll need to manually remove agent_font_size, which is replaced by
    agent_ui_font_size.

--- component/zed/zed.nix
+++ component/zed/zed.nix
@@ -21,23 +21,65 @@ in
     ];
 
     userSettings = {
+      # Keybindings
+      vim_mode = true;
+      helix_mode = true;
+
+      # UI
       theme = {
         mode = "system";
         light = "Ayu Light";
         dark = "Tokyo Night";
       };
+      icon_theme = "Material Icon Theme";
 
-      ui_font_size = 14;
-      agent_font_size = 14;
-      buffer_font_family = "MonaspiceKr NF";
-      buffer_font_size = 12;
+      # Fonts
+      ui_font_size = 16;
+      agent_ui_font_size = 14;
+      buffer_font_size = 14;
+      ui_font_family = "IBM Plex Sans";
+      buffer_font_family = "MonaspiceKr Nerd Font";
       terminal = {
-        font_family = "MonaspiceKr NF";
         font_size = 12;
+        font_family = "MonaspiceKr Nerd Font";
       };
 
-      vim_mode = true;
-      helix_mode = true;
+      # Edit predictions
+      show_edit_predictions = true;
+      edit_predictions = {
+        mode = "subtle";
+      };
+
+      # Zed Agent
+      agent = {
+        enabled = true;
+        use_modifier_to_send = true;
+        enable_feedback = false;
+        default_model = {
+          provider = "zed.dev";
+          model = "claude-sonnet-4-5";
+        };
+        default_profile = "write";
+        profiles = {};
+        always_allow_tool_actions = false;
+      };
+
+      # Other agents
+      agent_servers = {
+        OpenCode = {
+          type = "custom";
+          command = "opencode";
+          args = [ "acp" ];
+        };
+      };
+
+      # Collaboration
+      audio = {
+        "experimental.auto_microphone_volume" = true;
+      };
+      calls = {
+        "mute_on_join" = true;
+      };
     };
   };
 }
