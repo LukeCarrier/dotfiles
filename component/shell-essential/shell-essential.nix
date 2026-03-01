c3eb8fe2d3b185c9fb3dbedfb11e9144c04c8439
Luke Carrier <luke@carrier.family>
2026-03-01 00:46:20 +0000

    component/shell-essential: bat
    Not entirely sure I'll use it over cat/hx, but we'll give it a go.

--- component/shell-essential/shell-essential.nix
+++ component/shell-essential/shell-essential.nix
@@ -31,6 +31,14 @@
         [ ]
     );
 
+  programs.bat = {
+    enable = true;
+    extraPackages = with pkgs.bat-extras; [ batdiff batman batgrep batwatch ];
+    config = {
+      theme = "base16-256";
+    };
+  };
+
   home.sessionVariables = {
     DO_NOT_TRACK = "1";
   };
