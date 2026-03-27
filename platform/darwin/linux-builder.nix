4a1fc8d801e075c492f43e02ad4c03eb531de89f
Luke Carrier <luke@carrier.family>
2026-03-27 01:49:05 +0000

    platform/darwin/linux-builder: constrain machine to maxJobs = 1

--- platform/darwin/linux-builder.nix
+++ platform/darwin/linux-builder.nix
@@ -8,6 +8,12 @@
 
   nix = {
     distributedBuilds = true;
+    buildMachines =
+      lib.mkAfter (map (machine:
+        if machine.hostName == "rosetta-builder"
+        then machine // { maxJobs = 1; }
+        else machine
+      ) config.nix.buildMachines);
 
     settings.trusted-users = [ "@admin" ];
   };
