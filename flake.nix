e3970e234dd2206c2c07dfdb132727fa0e36d106
Luke Carrier <luke@carrier.family>
2026-04-13 23:33:39 +0100

    flake: add librewolf-unwrapped dbgsym workaround overlay

--- flake.nix
+++ flake.nix
@@ -132,6 +132,14 @@
               opencode = opencode.packages.${system}.default;
               # opencode-desktop = opencode.packages.${system}.desktop;
               goose-cli = final.callPackage ./package/goose-cli { };
+              # https://github.com/NixOS/nixpkgs/pull/480923#issuecomment-3804604919
+              librewolf-unwrapped = prev.librewolf-unwrapped.overrideAttrs (old: {
+                configureFlags = (old.configureFlags or [ ]) ++ [
+                  "--disable-debug"
+                  "--disable-debug-symbols"
+                ];
+                separateDebugInfo = false;
+              });
             })
             (
               final: prev:
