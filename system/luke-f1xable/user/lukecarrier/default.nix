437fab49a9b2c378e80c6525eba828a580d26525
Luke Carrier <luke@carrier.family>
2026-05-26 23:06:05 +0100

    system/luke-f1xable/user/lukecarrier: install OpenCode again

--- system/luke-f1xable/user/lukecarrier/default.nix
+++ system/luke-f1xable/user/lukecarrier/default.nix
@@ -31,6 +31,7 @@
     ../../../../component/bitwarden/bitwarden.nix
     ../../../../component/handy/nixos-home.nix
     ../../../../component/goose/goose.nix
+    ../../../../component/opencode/opencode.nix
     ../../../../component/agentkit/agentkit.nix
     ../../../../component/openssh/openssh.nix
     ../../../../component/atuin/atuin.nix
