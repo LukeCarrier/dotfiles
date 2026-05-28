b1bfc55f1a31fd56ee12ef184f890485d8797b93
Luke Carrier <luke@carrier.family>
2026-05-28 09:49:28 +0100

    component/swayidle: try and workaround an inhibitor issue

--- component/swayidle/swayidle.nix
+++ component/swayidle/swayidle.nix
@@ -1,15 +1,25 @@
 { config, lib, pkgs, ... }:
 let
   inherit (lib) getExe getExe';
-  brightnessctl = "${getExe pkgs.brightnessctl}";
-  systemctl = "${getExe' pkgs.systemd "systemctl"}";
+  brightnessctl = getExe pkgs.brightnessctl;
+  systemctl = getExe' pkgs.systemd "systemctl";
+  lockTimeoutSeconds = 120;
   lockCmd = config.services.swayidle.events.lock;
 in
 {
   services.swayidle = {
     enable = true;
 
-    events.before-sleep = lockCmd;
+    events = {
+      before-sleep = lockCmd;
+    };
+
+    # swaywm/swayidle#198
+    # swayidle is trying to observe logind idle inhibitors whenever it enables
+    # timeouts, but as the change means that it will only connect to the bus if
+    # after-sleep, before-resume or idlehint has been specified, it ends up
+    # making all the sd_bus calls using a NULL bus pointer.
+    extraArgs = [ "idlehint" (toString lockTimeoutSeconds) ];
 
     timeouts = [
       {
@@ -18,7 +28,7 @@ in
         resumeCommand = "${brightnessctl} -rd rgb:kbd_backlight";
       }
       {
-        timeout = 120;
+        timeout = lockTimeoutSeconds;
         command = lockCmd;
       }
       {
