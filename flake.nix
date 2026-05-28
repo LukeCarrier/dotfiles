c7c696bb05a905cede86495522e05a81e68ae16e
Luke Carrier <luke@carrier.family>
2026-05-28 09:49:28 +0100

    flake: drop librewolf patch

--- flake.nix
+++ flake.nix
@@ -178,9 +178,6 @@
                 buildInputs = (old.buildInputs or []) ++ [ basePkgs.wtype ];
                 patches = (old.patches or []) ++ [ ./package/handy/pr-1337.patch ];
               });
-              librewolf = prev.librewolf.overrideAttrs (old: {
-                patches = (old.patches or []) ++ [ ./package/firefox/fractional-scale.patch ];
-              });
               niri = niri.packages.${system}.niri-unstable;
             })
             (
