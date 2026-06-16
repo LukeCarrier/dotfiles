ffd4a0c65ef9c87bc91cac5bcb33b1d47c9c93d1
Luke Carrier <luke@carrier.family>
2026-06-16 09:54:09 +0100

    system/b-4653/user/luke.carrier: more harnesses

--- system/b-4653/user/luke.carrier/default.nix
+++ system/b-4653/user/luke.carrier/default.nix
@@ -30,6 +30,8 @@ in
     ../../../../component/1password/1password.nix
     ../../../../component/goose/goose.nix
     ../../../../component/opencode/opencode.nix
+    ../../../../component/claude-code/claude-code.nix
+    ../../../../component/codex/codex.nix
     ../../../../component/openssh/openssh.nix
     ../../../../component/atuin/atuin.nix
     ../../../../component/starship/starship.nix
