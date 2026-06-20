682200625f23c801e9e07fec52ef5519846e0242
Luke Carrier <luke@carrier.family>
2026-06-20 09:31:04 +0100

    component/swayidle: don't both with systemd
    As far as I can tell, this should not be necessary if the compositor
    implements the ext-idle-notify protocol?

--- component/swayidle/swayidle.nix
+++ component/swayidle/swayidle.nix
@@ -1,8 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  ...
+}:
 let
-  inherit (lib) getExe getExe';
-  systemctl = getExe' pkgs.systemd "systemctl";
-  lockTimeoutSeconds = 120;
   lockCmd = config.services.swayidle.events.lock;
 in
 {
@@ -13,21 +14,10 @@ in
       before-sleep = lockCmd;
     };
 
-    # swaywm/swayidle#198
-    # swayidle is trying to observe logind idle inhibitors whenever it enables
-    # timeouts, but as the change means that it will only connect to the bus if
-    # after-sleep, before-resume or idlehint has been specified, it ends up
-    # making all the sd_bus calls using a NULL bus pointer.
-    extraArgs = [ "idlehint" (toString lockTimeoutSeconds) ];
-
     timeouts = [
       {
         timeout = 120;
         command = lockCmd;
-      }
-      {
-        timeout = 300;
-        command = "${systemctl} suspend-then-hibernate";
       }
     ];
   };
