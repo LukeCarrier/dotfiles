bcec5629ca8d5a85b118ff27e1e8a4b9e081ba38
Luke Carrier <luke@carrier.family>
2026-08-06 09:52:41 +0100

    hw/thinkpad-l14: enable swap

--- hw/thinkpad-l14.nix
+++ hw/thinkpad-l14.nix
@@ -16,5 +16,12 @@
     ACTION=="add", SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_NAME}=="BAT0", ATTR{charge_control_end_threshold}="80"
   '';
 
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 64 * 1024;
+    }
+  ];
+
   services.hardware.bolt.enable = true;
 }
