d461652f5118e82fc006cbbdd92410f2026fc15f
Luke Carrier <luke@carrier.family>
2026-09-19 22:23:19 +0100

    hw/framework-13-amd: reset the touchpad on resume from suspend

--- hw/framework-13-amd.nix
+++ hw/framework-13-amd.nix
@@ -12,9 +12,17 @@ let
     fi
     echo "$auth" >"$AUTH_PATH"
   '';
+  resetTouchpad = pkgs.writeShellScriptBin "fw13-reset-touchpad" ''
+    modprobe -r i2c_hid_acpi
+    modprobe i2c_hid_acpi
+  '';
   resetWifi = pkgs.writeShellScriptBin "fw13-reset-wifi" ''
     modprobe -r mt7921e
     modprobe mt7921e
+  '';
+  postResume = pkgs.writeShellScriptBin "fw13-post-resume" ''
+    ${getExe resetTouchpad}
+    ${getExe resetWifi}
   '';
 in
 {
@@ -32,7 +40,7 @@ in
     lidEventCommands = ''${getExe lidFprint}'';
   };
 
-  systemd.services.fw13-post-resume-reset-wifi = {
+  systemd.services.fw13-post-resume = {
     description = "Local system resume actions";
     after = [
       "suspend.target"
@@ -45,7 +53,7 @@ in
 
     serviceConfig = {
       Type = "simple";
-      ExecStart = "${getExe resetWifi}";
+      ExecStart = "${getExe postResume}";
     };
   };
 
